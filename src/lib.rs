eb82f1570d8d23ec374c4457681ccf2d273c589d
sircodrin <codrin.sirboiu@gmail.com>
2024-11-03 15:10:30 +0200

    update

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ sol_storage! {
 
 #[public]
 impl Oracle {
-    pub fn calculate_average(&mut self, vec: [U256; 1000]) {
+    pub fn calculate_average(&mut self, vec: Vec<U256>) {
         let sum: U256 = vec.iter().sum();
         let average: U256 = sum / U256::from(vec.len());
 
